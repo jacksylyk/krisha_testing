e8ad2510cb6e89409cbd3b5268b0552f92e3a68a
jacksylyk <jacksylyk@dss.kz>
2024-05-28 15:06:01 +0500

    Added pytest-fixture to registration test

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,6 @@
 import unittest
 import allure
+import pytest
 from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,39 +13,42 @@ from pages.base_page import BasePage
 from pages.login_page import LoginPage
 
 
-class TestRegistration(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.login_page = LoginPage(self.driver)
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
 
-    def tearDown(self):
-        self.driver.quit()
 
-    @allure.title("Test: Авторизация")
-    def test_registration_link(self):
-        driver = self.driver
+@pytest.fixture
+def login_page(driver):
+    return LoginPage(driver)
+
+
+class TestRegistration:
+    @pytest.mark.usefixtures("driver", "login_page")
+    def test_registration_link(self, driver, login_page):
         base_page = BasePage(driver)
-        login_page = LoginPage(driver)
 
         with allure.step("Перейти на страницу krisha.kz"):
             base_page.navigate_to_url("https://krisha.kz")
         with allure.step("Нажать на кнопку Регистрация"):
             base_page.click_element(By.CLASS_NAME, "registration-link-item")
 
-        self.assertIn("Войти на Krisha.kz", driver.title)
+        assert "Войти на Krisha.kz" in driver.title
 
         login_page.enter_login(config.number_login)
-        self.assertEqual(login_page.get_login_value(), config.number_login)
+        assert login_page.get_login_value() == config.number_login
 
         login_page.click_button(By.XPATH, "/html/body/div[1]/div/div[2]/form/div[5]/div/button")
         time.sleep(1)
 
         login_page.enter_password(config.password_login)
-        self.assertEqual(login_page.get_password_value(), config.password_login)
+        assert login_page.get_password_value() == config.password_login
 
         login_page.click_button(By.XPATH, "/html/body/div[1]/div/div[2]/form/div[5]/div/button")
 
-        allure.attach(self.driver.get_screenshot_as_png(), name="Registration Page", attachment_type=AttachmentType.PNG)
+        allure.attach(driver.get_screenshot_as_png(), name="Registration Page", attachment_type=AttachmentType.PNG)
 
 
 if __name__ == "__main__":
